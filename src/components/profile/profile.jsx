d4e3e936a2dddd6e1cdb4150653342d4c36ae5fb
di0rio <caua.diorio29@gmail.com>
2023-08-10 17:11:48 -0300

    finalizando aula 10/08

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,8 @@
+export default function profile(){
+    return(
+        <div className="profile">
+            <img src="" alt="" />
+            <p>@di0rio</p>
+        </div>
+    )
+}
---
+++ src/pages/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Profile from "../components/profile/profile";
+
+export default function App() {
+    return (
+        <>
+        <Profile></Profile>
+        
+        {/* <div className="react">Olá, REACT!</div>
+        <div>Olá!</div> */}
+        </>
+    );
+}
