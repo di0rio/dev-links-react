49aa693610ff7bb78bd1285ed97a5bc13ca7e8a2
di0rio <caua.diorio29@gmail.com>
2023-08-11 10:20:18 -0300

    desenvolvido o profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,22 @@
-import "./App.css"
+import { useState } from "react";
 import Profile from "../components/profile/profile";
 
-export default function App() {
-    return (
-        <>
-        <Profile></Profile>
-        
-        {/* <div className="react">Olá, REACT!</div>
-        <div>Olá!</div> */}
-        </>
+import "./App.css"
+
+import imgDarkMode from "../img/motinha.jpg"
+import imgLightMode from "../img/imagemFundoBranco.jpg"
+
+const App = () => {
+const [isLightMode,setIsLightMode] = useState(false)
+
+const imgScr = isLightMode ? imgLightMode : imgDarkMode;
+const imgAlt = isLightMode ? "Imagem no modo CLaro" : "Imagem no modo Escuro"
+
+    return(
+        <div>
+            <Profile imgSrc={imgScr} imgAlt={imgAlt}>@di0rio</Profile>
+        </div>
     );
-}
+};
+
+export default App;
